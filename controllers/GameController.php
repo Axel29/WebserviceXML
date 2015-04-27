1285c644a894eb6e204c3cddba69caba741a5684
Axel29 <axel.bouaziz@hotmail.fr>
2015-04-27 11:55:07 +0200

    Validation XML par rapport au schema XSD

--- controllers/GameController.php
+++ controllers/GameController.php
@@ -5,16 +5,20 @@ class Game extends BaseController
 	 * Show the full XML
 	 */
 	public function indexAction()
-	{
-		$this->loadLayout('xml');
-		
+	{		
 		$games = [
 			'1',
 			'2',
 		];
 		$this->list = $this->generateXml($games);
 
-		$this->render('index');
+		if ($errors = $this->validateXML($this->list->asXML())) {
+			$this->loadLayout();
+			echo $errors;
+		} else {
+			$this->loadLayout('xml');
+			$this->render('index');
+		}
 	}
 
 	/**
@@ -79,7 +83,7 @@ class Game extends BaseController
 						$supports = $console->addChild('supports');
 							$supports->addChild('support');
 
-						$console->addChild('release');
+						$console->addChild('release', '2015-01-25');
 
 						$editions = $console->addChild('editions');
 							$edition = $editions->addChild('edition');
@@ -113,18 +117,18 @@ class Game extends BaseController
 
 						$test = $console->addChild('test');
 							$test->addChild('report');
-							$test->addChild('date');
+							$test->addChild('date', '2015-01-22T11:33:33');
 							$test->addChild('userName');
-							$test->addChild('note');
+							$test->addChild('note', '1');
 
 							$comments = $test->addChild('comments');
 								$comment = $comments->addChild('comment');
 								$comment->addChild('text');
-								$comment->addChild('date');
+								$comment->addChild('date', '2015-01-22T11:33:33');
 								$comment->addChild('userName');
-								$comment->addChild('note');
-								$comment->addChild('like');
-								$comment->addChild('dislike');
+								$comment->addChild('note', '3');
+								$comment->addChild('like', '0');
+								$comment->addChild('dislike', '0');
 
 						$analyses = $test->addChild('analyses');
 							$analyse = $analyses->addChild('analyse');
@@ -138,19 +142,19 @@ class Game extends BaseController
 			$articles = $game->addChild('articles');
 				$article = $articles->addChild('article');
 				$article->addAttribute('type', 'news');
-					$articleConsoleNames = $article->addChild('consoleNames');
-						$articleConsoleNames->addChild('consoleName');
-						
+					$articleConsolesNames = $article->addChild('consolesNames');
+						$articleConsolesNames->addChild('consoleName');
+
 					$article->addChild('title');
 					$article->addChild('userName');
-					$article->addChild('date');
+					$article->addChild('date', '2015-01-22T11:33:33');
 
 			$medias = $game->addChild('medias');
 				$media = $medias->addChild('media');
 					$media->addAttribute('type', 'video');
 					$media->addAttribute('url', 'path/to/media');
-					$mediaConsoleNames = $media->addChild('consoleNames');
-						$mediaConsoleNames->addChild('consoleName');
+					$mediaConsolesNames = $media->addChild('consolesNames');
+						$mediaConsolesNames->addChild('consoleName');
 							$mediaDimensions = $media->addChild('dimensions');
 							$mediaDimensions->addAttribute('unit', 'pt');
 							$mediaDimensions->addAttribute('height', '32.4');
@@ -158,11 +162,57 @@ class Game extends BaseController
 
 			$tips = $game->addChild('tips');
 				$tip = $tips->addChild('tip');
-				$tipConsoleNames = $tip->addChild('consoleNames');
-					$tipConsoleNames->addChild('consoleNames');
+				$tipConsoleNames = $tip->addChild('consolesNames');
+					$tipConsoleNames->addChild('consoleName');
 					$tip->addChild('content');
 		}
 
 		return $list;
+	}
+
+	/**
+	 * Validate XML from XSD
+	 *
+	 * @param $xml SimpleXMLElement XML to validate
+	 * @return $result string Errors to display or empty string
+	 */
+	public function validateXML($xml)
+	{
+		// Enable user error handling
+		libxml_use_internal_errors(true);
+
+		$domDocument = new DOMDocument();
+		$domDocument->loadXML($xml);
+
+		$result = '';
+
+		if (!$domDocument->schemaValidate(ROOT . 'core/schemavideogame.xsd')) {
+			$errors = libxml_get_errors();
+
+			foreach ($errors as $error) {
+				$result = "<br/>\n";
+				switch ($error->level) {
+					case LIBXML_ERR_WARNING:
+						$result .= "<b>Warning $error->code</b>: ";
+					break;
+					case LIBXML_ERR_ERROR:
+						$result .= "<b>Error $error->code</b>: ";
+					break;
+					case LIBXML_ERR_FATAL:
+						$result .= "<b>Fatal Error $error->code</b>: ";
+					break;
+				}
+
+				$result .= trim($error->message);
+
+				if ($error->file) {
+					$result .= " in <b>$error->file</b>";
+				}
+				$result .= " on line <b>$error->line</b>\n";
+			}
+			libxml_clear_errors();
+		}
+
+		return $result;
 	}
 }
