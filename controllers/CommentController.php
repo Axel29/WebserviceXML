f2ef38bc215055ebf38b3c6e1521bca3f4e2bbbc
Axel29 <axel.bouaziz@hotmail.fr>
2015-04-30 12:02:41 +0200

    CRUD Comment

---
+++ controllers/CommentController.php
@@ -0,0 +1,259 @@
+<?php
+/**
+ * @todo Vérifier que l'ID du test existe avant d'insérer ou de mettre à jour.
+ */
+class CommentController extends BaseController
+{
+	/**
+	 * @var $id int Game's ID
+	 */
+	private $id = null;
+
+	/**
+	 * @var $requiredFields array Required fields and their types for insert / update
+	 */
+	private $requiredFields = [
+		'date'        => 'date',
+		'user_name'   => 'string',
+		'note'        => 'int',
+		'like'        => 'int',
+		'dislike'     => 'int',
+		'text'        => 'string',
+		'test_idTest' => 'int',
+	];
+
+	/**
+	 * Redirect the request to the matching method regarding the request method
+	 * Route: /comment/id/{id}
+	 *
+	 * @param $id int ID of the comment. Used for POST, PUT and DELETE methods
+	 */
+	public function indexAction($id = null)
+	{
+		if ($id) {
+			$this->setId($id);
+		}
+		switch ($this->getRequestMethod()) {
+			case 'GET':
+				$this->show();
+				break;
+			case 'POST':
+				$this->add();
+				break;
+			case 'PUT':
+				$this->update();
+				break;
+			case 'DELETE':
+				$this->delete();
+				break;
+			
+			default:
+				$this->show();
+				break;
+		}
+	}
+
+	/**
+	 * Show the full comments list or a specific comment by it's ID
+	 *
+	 * @param $id int Comment's ID
+	 */
+	public function show()
+	{
+		$commentModel = new Comment();
+
+		// Show the full comment list or a specific comment by it's ID
+		$datas = $commentModel->findBy('idComment', $this->getId());
+
+		$this->xml = $this->generateXml($datas)->asXML();
+
+		if ($errors = $this->validateXML($this->xml)) {
+			$this->exitError(400, $errors);
+		} else {
+			$this->loadLayout('xml');
+			echo $this->xml;
+		}
+	}
+
+	/**
+	 * Add new comment
+	 * Route: /comment/add
+	 */
+	public function add()
+	{
+		// Security check for the request method
+		if ($this->getRequestMethod() != 'POST') {
+			$this->exitError(405, 'Only POST methods are allowed.');
+			return;
+		}
+
+		// Check every required field
+		$this->checkRequiredFields($this->requiredFields, $_POST, 'Y-m-d');
+
+		$commentModel    = new Comment();
+		$insertedComment = $commentModel->insertComment($_POST);
+
+		if ($insertedComment) {
+			$this->sendStatus(201);
+			return;
+		} else {
+			$this->exitError(400, 'An error has occurred. Please try again.');
+		}
+	}
+
+	/**
+	 * Update comment
+	 * Route: /comment/id/{id}
+	 */
+	public function update()
+	{
+		// Security check for the request method
+		if ($this->getRequestMethod() != 'PUT') {
+			$this->exitError(405, 'Only PUT methods are allowed.');
+			return;
+		}
+
+		// Get PUT datas
+		parse_str(file_get_contents("php://input"), $_PUT);
+		if (!$this->getId()) {
+			$this->exitError(400, "'id' must be specified.");
+		}
+
+		// Check every required field
+		$this->checkRequiredFields($this->requiredFields, $_PUT, 'Y-m-d');
+
+		$commentModel  = new Comment();
+		$updatedComment = $commentModel->updateComment($this->getId(), $_PUT);
+
+		if ($updatedComment) {
+			$this->sendStatus(204);
+			return;
+		} else {
+			$this->exitError(400, 'An error has occurred. Please try again.');
+		}
+	}
+
+	/**
+	 * Delete game
+	 * Route: /comment/delete/id/{id}
+	 *
+	 * @param $id int Comment's ID to delete
+	 */
+	public function delete()
+	{		
+		// Security check for the request method
+		if (!$this->getRequestMethod() == 'DELETE') {
+			$this->exitError(405, 'Only DELETE methods are allowed.');
+			return;
+		}
+
+		if (!$this->getId()) {
+			$this->exitError(400, "'id' must be specified.");
+		}
+
+		$commentModel   = new Comment();
+		$deletedComment = $commentModel->deleteComment($this->getId());
+
+		if ($deletedComment) {
+			$this->sendStatus(204);
+		} else {
+			$this->exitError(400, 'An error has occured. Please try again.');
+		}
+	}
+
+	/**
+	 * Generate XML from database.
+	 * Can generate either the entire database's XML, either only one comment's XML.
+	 *
+	 * @param array $comment Comment to insert in the XML
+	 * @return SimpleXMLElement $comments List of comments or game
+	 */
+	public function generateXml($comments = [])
+	{
+		$list = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><comments/>');
+		foreach ($comments as $comment) {
+			$commentNode = $list->addChild('comment');
+			$commentNode->addAttribute('id', $comment['idComment']);
+			$commentNode->addChild('text', $comment['text']);
+			$commentNode->addChild('date', $comment['date']);
+			$commentNode->addChild('userName', $comment['user_name']);
+			$commentNode->addChild('note', $comment['note']);
+			$commentNode->addChild('like', $comment['like']);
+			$commentNode->addChild('dislike', $comment['dislike']);
+		}
+		// $this->loadLayout('xml');
+		// echo($list->asXML());
+		// die;
+		return $list;
+	}
+
+	/**
+	 * Validate XML from XSD
+	 *
+	 * @param $xml SimpleXMLElement XML to validate
+	 * @return $result string Errors to display or empty string
+	 */
+	public function validateXML($xml)
+	{
+		// Enable user error handling
+		libxml_use_internal_errors(true);
+
+		$domDocument = new DOMDocument();
+		$domDocument->loadXML($xml);
+
+		$result = '';
+
+		if (!$domDocument->schemaValidate(SCHEMAS_PATH . 'comments.xsd')) {
+			$errors = libxml_get_errors();
+
+			foreach ($errors as $error) {
+				$result = "<br/>\n";
+				switch ($error->level) {
+					case LIBXML_ERR_WARNING:
+						$result .= "<b>Warning $error->code</b>: ";
+					break;
+					case LIBXML_ERR_ERROR:
+						$result .= "<b>Error $error->code</b>: ";
+					break;
+					case LIBXML_ERR_FATAL:
+						$result .= "<b>Fatal Error $error->code</b>: ";
+					break;
+				}
+
+				$result .= trim($error->message);
+
+				if ($error->file) {
+					$result .= " in <b>$error->file</b>";
+				}
+				$result .= " on line <b>$error->line</b>\n";
+			}
+			libxml_clear_errors();
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Get the ID
+	 *
+	 * @return int
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	/**
+	 * Set the ID
+	 *
+	 * @param $id int
+	 */
+	public function setId($id)
+	{
+		if ($this->isInt($id)) {
+			$this->id = $id;
+		} else {
+			$this->exitError(400, sprintf("'id' must be an integer, %s given", gettype($id)));
+		}
+	}
+}
---
+++ model/Comment.php
@@ -0,0 +1,135 @@
+<?php
+class Comment extends BaseModel
+{
+	/**
+	 * Retrieve every available comments or comments by some param
+	 *
+	 * @param $paramName string Param's name to find by
+	 * @param $paramValue mixed Param's value
+	 * @return $comments array
+	 */
+	public function findBy($paramName = null, $paramValue = null)
+	{
+		$this->table = 'comment c';
+		
+		$fields = [
+			'idComment',
+			'DATE_FORMAT(`date`, "%Y-%m-%dT%H:%i:%s") as `date`',
+			'`user_name`',
+			'`note`',
+			'`like`',
+			'`dislike`',
+			'`text`',
+			'`test_idTest`',
+		];
+
+		$where = [];
+		if ($paramName && $paramValue) {
+			$where = [
+				'c.' . $paramName => $paramValue,
+			];
+		}
+
+		$comments = $this->select($fields, $where);
+
+		return $comments;
+	}
+
+	/**
+	 * Insert a new comment in database.
+	 *
+	 * @param $datas array Comment's name
+	 * @return $id int Comment's ID
+	 */
+	public function insertComment($datas)
+	{
+		try {
+			$pdo  = $this->db;
+			$stmt = $pdo->prepare('INSERT INTO `comment` (`date`, `user_name`, `note`, `like`, `dislike`, `text`, `test_idTest`) 
+								   VALUES (:date, :user_name, :note, :like, :dislike, :text, :test_idTest)');
+			$stmt->bindParam(':date', $datas['date'], PDO::PARAM_STR);
+			$stmt->bindParam(':user_name', $datas['user_name'], PDO::PARAM_STR);
+			$stmt->bindParam(':note', $datas['note'], PDO::PARAM_INT);
+			$stmt->bindParam(':like', $datas['like'], PDO::PARAM_INT);
+			$stmt->bindParam(':dislike', $datas['dislike'], PDO::PARAM_INT);
+			$stmt->bindParam(':text', $datas['text'], PDO::PARAM_STR);
+			$stmt->bindParam(':test_idTest', $datas['test_idTest'], PDO::PARAM_INT);
+			$stmt->execute();
+
+			return $pdo->lastInsertId();
+		} catch (PDOException $e) {
+			return false;
+		} catch (Exception $e) {
+			return false;
+		}
+	}
+
+	/**
+	 * Update comment
+	 *
+	 * @param $idComment int Comment's ID
+	 * @param $datas array Datas to update
+	 */
+	public function updateComment($idComment, $datas)
+	{
+		try {
+			$pdo  = $this->db;
+			$stmt = $pdo->prepare('UPDATE `comment` 
+								   SET `date` = :date,
+								       `user_name` = :user_name,
+								       `note` = :note,
+								       `like` = :like,
+								       `dislike` = :dislike,
+								       `text` = :text,
+								       `test_idTest` = :test_idTest
+								   WHERE `idComment` =  :idComment');
+			$stmt->bindParam(':date', $datas['date'], PDO::PARAM_STR);
+			$stmt->bindParam(':user_name', $datas['user_name'], PDO::PARAM_STR);
+			$stmt->bindParam(':note', $datas['note'], PDO::PARAM_INT);
+			$stmt->bindParam(':like', $datas['like'], PDO::PARAM_INT);
+			$stmt->bindParam(':dislike', $datas['dislike'], PDO::PARAM_INT);
+			$stmt->bindParam(':text', $datas['text'], PDO::PARAM_STR);
+			$stmt->bindParam(':test_idTest', $datas['test_idTest'], PDO::PARAM_INT);
+			$stmt->bindParam(':idComment', $idComment, PDO::PARAM_INT);
+			$stmt->execute();
+			/*
+			 * Check that the update was performed on an existing comment.
+			 * MySQL won't send any error as, regarding to him, the request is correct, so we have to handle it manually.
+			 */
+			return $stmt->rowCount();
+		} catch (PDOException $e) {
+			echo $e->getMessage();
+			return false;
+		} catch (Exception $e) {
+			echo $e->getMessage();
+			return false;
+		}
+	}
+
+	/**
+	 * Delete a comment by it's ID
+	 *
+	 * @param $id int Comment's ID
+	 */
+	public function deleteComment($idComment)
+	{
+		try {
+			$pdo  = $this->db;
+			$stmt = $pdo->prepare('DELETE 
+								   FROM `comment` 
+								   WHERE `idComment` =  :idComment');
+			$stmt->bindParam(':idComment', $idComment, PDO::PARAM_INT);
+			$stmt->execute();
+
+			/*
+			 * Check that the update was performed on an existing comment.
+			 * MySQL won't send any error as, regarding to him, the request is correct, so we have to handle it manually.
+			 */
+			return $stmt->rowCount();
+		} catch (PDOException $e) {
+			return false;
+		} catch (Exception $e) {
+			return false;
+		}
+	}
+}
