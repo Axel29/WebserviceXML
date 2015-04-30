e84a4bad94c5310227a1a3e08c583d73c0965867
Axel29 <axel.bouaziz@hotmail.fr>
2015-04-30 08:36:39 +0200

    Ajout variables globales DANS et SCHEMAS_PATH

--- core/conf.php
+++ core/conf.php
@@ -4,3 +4,6 @@ define('BASE_URL', 'http://'.$_SERVER['SERVER_NAME']);
 
 define('SITE_NAME', 'XML - Web Service');
 
+define('DS', DIRECTORY_SEPARATOR);
+define('SCHEMAS_PATH', ROOT . 'core' . DS . 'schemas' . DS);
+
