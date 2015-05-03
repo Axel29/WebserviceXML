3521fbfca8996b69f7973e577467bc592149289b
Axel29 <axel.bouaziz@hotmail.fr>
2015-05-03 14:32:29 +0200

    CRUD Tests

---
+++ controllers/TestController.php
@@ -0,0 +1,286 @@
+<?php
+/**
+ * @todo Vérifier que l'ID de la console existe avant d'insérer ou de mettre à jour.
+ */
+class TestController extends BaseController
+{
+	/**
+	 * @var $id int Test's ID
+	 */
+	private $id = null;
+
+	/**
+	 * @var $requiredFields array Required fields and their types for insert / update
+	 */
+	private $requiredFields = [
+		'report'            => 'string',
+		'date'              => 'string',
+		'user_name'         => 'string',
+		'note'              => 'int',
+		'console_idConsole' => 'int',
+	];
+
+	/**
+	 * Redirect the request to the matching method regarding the request method
+	 * Route: /test/id/{id}
+	 *
+	 * @param $id int ID of the test. Used for POST, PUT and DELETE methods
+	 */
+	public function indexAction($id = null)
+	{
+		if ($id) {
+			$this->setId($id);
+		}
+		switch ($this->getRequestMethod()) {
+			case 'GET':
+				$this->show();
+				break;
+			case 'POST':
+				$this->add();
+				break;
+			case 'PUT':
+				$this->update();
+				break;
+			case 'DELETE':
+				$this->delete();
+				break;
+			
+			default:
+				$this->show();
+				break;
+		}
+	}
+
+	/**
+	 * Show the full tests list or a specific test by it's ID
+	 *
+	 * @param $id int Test's ID
+	 */
+	public function show()
+	{
+		$testModel = new Test();
+
+		// Show the full test list or a specific test by it's ID
+		$datas = $testModel->findBy('idTest', $this->getId());
+
+		$this->xml = $this->generateXml($datas)->asXML();
+
+		if ($errors = $this->validateXML($this->xml)) {
+			$this->exitError(400, $errors);
+		} else {
+			$this->loadLayout('xml');
+			echo $this->xml;
+		}
+	}
+
+	/**
+	 * Add new test
+	 * Route: /test/add
+	 */
+	public function add()
+	{
+		// Security check for the request method
+		if ($this->getRequestMethod() != 'POST') {
+			$this->exitError(405, 'Only POST methods are allowed.');
+			return;
+		}
+
+		// Check every required field
+		$this->checkRequiredFields($this->requiredFields, $_POST);
+
+		$testModel    = new Test();
+		$insertedTest = $testModel->insertTest($_POST);
+
+		if ($insertedTest) {
+			$this->sendStatus(201);
+			return;
+		} else {
+			$this->exitError(400, 'An error has occurred. Please try again.');
+		}
+	}
+
+	/**
+	 * Update test
+	 * Route: /test/id/{id}
+	 */
+	public function update()
+	{
+		// Security check for the request method
+		if ($this->getRequestMethod() != 'PUT') {
+			$this->exitError(405, 'Only PUT methods are allowed.');
+			return;
+		}
+
+		// Get PUT datas
+		parse_str(file_get_contents("php://input"), $_PUT);
+		if (!$this->getId()) {
+			$this->exitError(400, "'id' must be specified.");
+		}
+
+		// Check every required field
+		$this->checkRequiredFields($this->requiredFields, $_PUT);
+
+		$testModel  = new Test();
+		$updatedTest = $testModel->updateTest($this->getId(), $_PUT);
+
+		if ($updatedTest) {
+			$this->sendStatus(204);
+			return;
+		} else {
+			$this->exitError(400, 'An error has occurred. Please try again.');
+		}
+	}
+
+	/**
+	 * Delete test
+	 * Route: /test/delete/id/{id}
+	 *
+	 * @param $id int Test's ID to delete
+	 */
+	public function delete()
+	{		
+		// Security check for the request method
+		if (!$this->getRequestMethod() == 'DELETE') {
+			$this->exitError(405, 'Only DELETE methods are allowed.');
+			return;
+		}
+
+		if (!$this->getId()) {
+			$this->exitError(400, "'id' must be specified.");
+		}
+
+		$testModel   = new Test();
+
+        // Check that there is at least one test left before deleting, otherwise, the XML would be broken.
+        if ($testModel->getNumberOfTestsLeft($this->getId()) > 1) {
+            $deletedTest = $testModel->deleteTest($this->getId());
+
+            if ($deletedTest) {
+                $this->sendStatus(204);
+            } else {
+                $this->exitError(400, 'An error has occured. Please try again.');
+            }
+        } else {
+            $this->exitError(400, 'There must be at least one test per console.');
+        }
+	}
+
+	/**
+	 * Generate XML from database.
+	 * Can generate either the entire database's XML, either only one test's XML.
+	 *
+	 * @param array $test Test to insert in the XML
+	 * @return SimpleXMLElement $tests List of tests or test
+	 */
+	public function generateXml($tests = [])
+	{
+		$list = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><tests/>');
+		foreach ($tests as $test) {
+			$testNode = $list->addChild('test');
+			$testNode->addAttribute('id', $test['idTest']);
+			$testNode->addChild('report', $test['report']);
+			$testNode->addChild('date', $test['date']);
+			$testNode->addChild('userName', $test['user_name']);
+			$testNode->addChild('note', $test['note']);
+
+			$comments     = new Comment();
+			$commentsNode = $testNode->addChild('comments');
+			if ($comments = $comments->findBy('test_idTest', $test['idTest'])) {
+				foreach ($comments as $comment) {
+					$commentNode = $commentsNode->addChild('comment');
+					$commentNode->addAttribute('id', $comment['idComment']);
+					$commentNode->addChild('text', $comment['text']);
+					$commentNode->addChild('date', $comment['date']);
+					$commentNode->addChild('userName', $comment['user_name']);
+					$commentNode->addChild('note', $comment['note']);
+					$commentNode->addChild('like', $comment['like']);
+					$commentNode->addChild('dislike', $comment['dislike']);
+				}
+			}
+
+			$analyses     = new Analyse();
+			$analysesNode = $testNode->addChild('analyses');
+			if ($analyses = $analyses->findBy('test_idTest', $test['idTest'])) {
+				foreach ($analyses as $analyse) {
+					$analyseNode = $analysesNode->addChild('analyse');
+					$analyseNode->addAttribute('id', $analyse['idAnalyse']);
+					$analyseNode->addAttribute('type', $analyse['type']);
+				}
+			}
+		}
+		// $this->loadLayout('xml');
+		// echo($list->asXML());
+		// die;
+		return $list;
+	}
+
+	/**
+	 * Validate XML from XSD
+	 *
+	 * @param $xml SimpleXMLElement XML to validate
+	 * @return $result string Errors to display or empty string
+	 */
+	public function validateXML($xml)
+	{
+		// Enable user error handling
+		libxml_use_internal_errors(true);
+
+		$domDocument = new DOMDocument();
+		$domDocument->loadXML($xml);
+
+		$result = '';
+
+		if (!$domDocument->schemaValidate(SCHEMAS_PATH . 'tests.xsd')) {
+			$errors = libxml_get_errors();
+
+			foreach ($errors as $error) {
+				$result = "<br/>\n";
+				switch ($error->level) {
+					case LIBXML_ERR_WARNING:
+						$result .= "<b>Warning $error->code</b>: ";
+					break;
+					case LIBXML_ERR_ERROR:
+						$result .= "<b>Error $error->code</b>: ";
+					break;
+					case LIBXML_ERR_FATAL:
+						$result .= "<b>Fatal Error $error->code</b>: ";
+					break;
+				}
+
+				$result .= trim($error->message);
+
+				if ($error->file) {
+					$result .= " in <b>$error->file</b>";
+				}
+				$result .= " on line <b>$error->line</b>\n";
+			}
+			libxml_clear_errors();
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Get the ID
+	 *
+	 * @return int
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	/**
+	 * Set the ID
+	 *
+	 * @param $id int
+	 */
+	public function setId($id)
+	{
+		if ($this->isInt($id)) {
+			$this->id = $id;
+		} else {
+			$this->exitError(400, sprintf("'id' must be an integer, %s given", gettype($id)));
+		}
+	}
+}
---
+++ model/Test.php
@@ -0,0 +1,160 @@
+<?php
+class Test extends BaseModel
+{
+	/**
+	 * Retrieve every available tests or tests by some param
+	 *
+	 * @param $paramName string Param's name to find by
+	 * @param $paramValue mixed Param's value
+	 * @return $tests array
+	 */
+	public function findBy($paramName = null, $paramValue = null)
+	{
+		$this->table = 'test t';
+		
+		$fields = [
+			'idTest',
+			'`report`',
+			'DATE_FORMAT(`date`, "%Y-%m-%dT%H:%i:%s") as `date`',
+			'`user_name`',
+			'`note`',
+			'`console_idConsole`',
+		];
+
+		$where = [];
+		if ($paramName && $paramValue) {
+			$where = [
+				$paramName => $paramValue,
+			];
+		}
+
+		$tests = $this->select($fields, $where);
+
+		return $tests;
+	}
+
+    /**
+     * Retrieve number of available tests for a console by test ID.
+     * Used to check that there is at least one test left before deleting as they are required.
+     *
+     * @param $testId int Test's ID
+     * @return $result['nbrTest'] int Number of existing tests
+     */
+    public function getNumberOfTestsLeft($testId)
+    {
+        try {
+            $pdo  = $this->db;
+            $stmt = $pdo->prepare('SELECT COUNT(`t`.`idTest`) AS `nbrTests`
+								   FROM `test` `t`
+								   LEFT JOIN `console` `c` ON `t`.`console_idConsole` = `c`.`idConsole`
+								   LEFT JOIN `game` `g` ON `c`.`game_idGame` = `g`.`idGame`
+								   WHERE `c`.`idConsole` = (SELECT `t`.`console_idConsole`
+                                                             FROM `test` `t`
+                                                             WHERE `t`.`idTest` = :idTest
+                                                            )
+                                  ');
+            $stmt->bindParam(':idTest', $testId, PDO::PARAM_INT);
+            $stmt->execute();
+
+            $result = $stmt->fetch();
+            return $result['nbrTests'];
+        } catch (PDOException $e) {
+            return false;
+        } catch (Exception $e) {
+            return false;
+        }
+    }
+
+	/**
+	 * Insert a new test in database.
+	 *
+	 * @param $datas array Test's name
+	 * @return $id int Test's ID
+	 */
+	public function insertTest($datas)
+	{
+		try {
+			$pdo  = $this->db;
+			$stmt = $pdo->prepare('INSERT INTO `test` (`report`, `date`, `user_name`, `note`, `console_idConsole`) 
+								   VALUES (:report, :date, :user_name, :note, :console_idConsole)');
+			$stmt->bindParam(':report', $datas['report'], PDO::PARAM_STR);
+			$stmt->bindParam(':date', $datas['date'], PDO::PARAM_STR);
+			$stmt->bindParam(':user_name', $datas['user_name'], PDO::PARAM_STR);
+			$stmt->bindParam(':note', $datas['note'], PDO::PARAM_INT);
+			$stmt->bindParam(':console_idConsole', $datas['console_idConsole'], PDO::PARAM_INT);
+			$stmt->execute();
+
+			return $pdo->lastInsertId();
+		} catch (PDOException $e) {
+			echo $e->getMessage();
+			return false;
+		} catch (Exception $e) {
+			return false;
+		}
+	}
+
+	/**
+	 * Update test
+	 *
+	 * @param $idTest int Test's ID
+	 * @param $datas array Datas to update
+	 */
+	public function updateTest($idTest, $datas)
+	{
+		try {
+			$pdo  = $this->db;
+			$stmt = $pdo->prepare('UPDATE `test`
+								   SET `report` = :report,
+								   	   `date` = :date,
+								       `user_name` = :user_name,
+								       `note` = :note,
+								       `console_idConsole` = :console_idConsole
+								   WHERE `idTest` =  :idTest');
+			$stmt->bindParam(':report', $datas['report'], PDO::PARAM_STR);
+			$stmt->bindParam(':date', $datas['date'], PDO::PARAM_STR);
+			$stmt->bindParam(':user_name', $datas['user_name'], PDO::PARAM_STR);
+			$stmt->bindParam(':note', $datas['note'], PDO::PARAM_INT);
+			$stmt->bindParam(':console_idConsole', $datas['console_idConsole'], PDO::PARAM_INT);
+			$stmt->bindParam(':idTest', $idTest, PDO::PARAM_INT);
+			$stmt->execute();
+			/*
+			 * Check that the update was performed on an existing test.
+			 * MySQL won't send any error as, regarding to him, the request is correct, so we have to handle it manually.
+			 */
+			return $stmt->rowCount();
+		} catch (PDOException $e) {
+			echo $e->getMessage();
+			return false;
+		} catch (Exception $e) {
+			echo $e->getMessage();
+			return false;
+		}
+	}
+
+	/**
+	 * Delete a test by it's ID
+	 *
+	 * @param $id int Test's ID
+	 */
+	public function deleteTest($idTest)
+	{
+		try {
+			$pdo  = $this->db;
+			$stmt = $pdo->prepare('DELETE 
+								   FROM `test` 
+								   WHERE `idTest` =  :idTest');
+			$stmt->bindParam(':idTest', $idTest, PDO::PARAM_INT);
+			$stmt->execute();
+
+			/*
+			 * Check that the update was performed on an existing test.
+			 * MySQL won't send any error as, regarding to him, the request is correct, so we have to handle it manually.
+			 */
+			return $stmt->rowCount();
+		} catch (PDOException $e) {
+			return false;
+		} catch (Exception $e) {
+			return false;
+		}
+	}
+}
