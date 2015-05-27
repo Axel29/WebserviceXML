9fb8cc3d9bab38a6e45d20f33d71a704d848463d
Axel29 <axel.bouaziz@hotmail.fr>
2015-05-27 21:29:30 +0200

    CRUD Languages

---
+++ controllers/LanguageController.php
@@ -0,0 +1,226 @@
+<?php
+class LanguageController extends BaseController
+{
+	/**
+	 * @var $id int Language's ID
+	 */
+	private $id = null;
+
+	/**
+	 * Redirect the request to the matching method regarding the request method
+	 * Route: /language/index/id/{id}
+	 *
+	 * @param $id int ID of the language. Used for POST, PUT and DELETE methods
+	 */
+	public function indexAction($id = null)
+	{
+		if ($id) {
+			$this->setId($id);
+		}
+
+		switch ($this->getRequestMethod()) {
+			case 'GET':
+				$this->show();
+				break;
+			case 'POST':
+				$this->add();
+				break;
+			case 'PUT':
+				$this->update();
+				break;
+			case 'DELETE':
+				$this->delete();
+				break;
+			
+			default:
+				$this->show();
+				break;
+		}
+	}
+
+	/**
+	 * Show the full languages list or a specific language by it's ID
+	 *
+	 * @param $id int Language's ID
+	 */
+	public function show()
+	{
+		$languageModel = new Language();
+
+		// Show the full language list or a specific language by it's ID
+		$datas = $languageModel->findBy('idLanguage', $this->getId());
+
+		$this->xml = $this->generateXml($datas)->asXML();
+
+		if ($errors = $this->validateXML($this->xml)) {
+			$this->exitError(400, $errors);
+		} else {
+			$this->loadLayout('xml');
+			echo $this->xml;
+		}
+	}
+
+	/**
+	 * Add new language
+	 * Route: /language
+	 */
+	public function add()
+	{
+		// Security check for the request method
+		if ($this->getRequestMethod() != 'POST') {
+			$this->exitError(405, 'Only POST methods are allowed.');
+			return;
+		}
+
+		// Check every required field
+		$requiredFields = [
+			'language' => 'string',
+		];
+
+		$this->checkRequiredFields($requiredFields, $_POST);
+
+		$languageModel    = new Language();
+		$insertedLanguage = $languageModel->insertLanguage($_POST);
+
+		if ($insertedLanguage) {
+			$this->sendStatus(201);
+			return;
+		} else {
+			$this->exitError(400, 'An error has occurred. Please try again.');
+		}
+	}
+
+	/**
+	 * Update language
+	 * Route: /language/index/id/{id}
+	 */
+	public function update()
+	{
+		// Security check for the request method
+		if ($this->getRequestMethod() != 'PUT') {
+			$this->exitError(405, 'Only PUT methods are allowed.');
+			return;
+		}
+
+		// Get PUT datas
+		parse_str(file_get_contents("php://input"), $_PUT);
+		if (!$this->getId()) {
+			$this->exitError(400, "'id' must be specified.");
+		}
+
+		// Check every required field
+		$requiredFields = [
+			'language' => 'string',
+		];
+
+		$this->checkRequiredFields($requiredFields, $_PUT);
+
+		$languageModel  = new Language();
+		$updateLanguage = $languageModel->updateLanguage($this->getId(), $_PUT);
+
+		if ($updateLanguage) {
+			$this->sendStatus(204);
+			return;
+		} else {
+			$this->exitError(400, 'An error has occurred. Please try again.');
+		}
+	}
+
+	/**
+	 * Delete language. Forbiddent action.
+	 * Route: /language/index/id/{id}
+	 *
+	 * @param $id int Language's ID to delete
+	 */
+	public function delete()
+	{		
+		$this->exitError(405, 'Languages deletion is not allowed.');
+	}
+
+	/**
+	 * Generate XML from database.
+	 * Can generate either the entire database's XML, either only one language's XML.
+	 *
+	 * @param array $language Language to insert in the XML
+	 * @return SimpleXMLElement $languages List of languages or language
+	 */
+	public function generateXml($languages = [])
+	{
+		$list = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><languages/>');
+		foreach ($languages as $language) {
+			$languageNode = $list->addChild('language', $language['language']);
+			$languageNode->addAttribute('id', $language['idLanguage']);
+		}
+		return $list;
+	}
+
+	/**
+	 * Validate XML from XSD
+	 *
+	 * @param $xml SimpleXMLElement XML to validate
+	 * @return $result string Errors to display or empty string
+	 */
+	public function validateXML($xml)
+	{
+		// Enable user error handling
+		libxml_use_internal_errors(true);
+
+		$domDocument = new DOMDocument();
+		$domDocument->loadXML($xml);
+
+		$result = '';
+
+		if (!$domDocument->schemaValidate(SCHEMAS_PATH . 'languages.xsd')) {
+			$errors = libxml_get_errors();
+
+			foreach ($errors as $error) {
+				$result = "<br>\n";
+				switch ($error->level) {
+					case LIBXML_ERR_WARNING:
+						$result .= "<strong>Warning $error->code</strong>: ";
+					break;
+					case LIBXML_ERR_ERROR:
+						$result .= "<strong>Error $error->code</strong>: ";
+					break;
+					case LIBXML_ERR_FATAL:
+						$result .= "<strong>Fatal Error $error->code</strong>: ";
+					break;
+				}
+
+				$result .= trim($error->message);
+
+				if ($error->file) {
+					$result .= " in <strong>$error->file</strong>";
+				}
+				$result .= " on line <strong>$error->line</strong>\n";
+			}
+			libxml_clear_errors();
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Get the ID
+	 *
+	 * @return int
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	/**
+	 * Set the ID
+	 *
+	 * @param $id int
+	 */
+	public function setId($id)
+	{
+		if ($this->isInt($id)) {
+			$this->id = $id;
+		} else {
+			$this->exitError(400, sprintf('The ID must be an integer. %s given', gettype($id)));
+		}
+	}
+}
---
+++ model/Language.php
@@ -0,0 +1,109 @@
+<?php
+class Language extends BaseModel
+{
+	/**
+	 * Retrieve every available languages or languages by some param
+	 *
+	 * @param $paramName string Param's name to find by
+	 * @param $paramValue mixed Param's value
+	 * @return $languages array
+	 */
+	public function findBy($paramName = null, $paramValue = null)
+	{
+		$this->table = 'language l';
+
+		$fields = ['`idLanguage`', '`language`'];
+		
+		$where = [];
+		$join  = [];
+		if ($paramName && $paramValue) {
+			if ($paramName == 'idGame') {
+				$join = [
+					[
+						'type'  => 'INNER JOIN',
+						'table' => 'game_has_language ghl',
+						'on'    => 'l.idLanguage = ghl.language_idLanguage',
+					],
+					[
+						'type'  => 'INNER JOIN',
+						'table' => 'game g',
+						'on'    => 'g.idGame = ghl.game_idGame',
+					],
+				];
+
+				$where = [
+					'ghl.game_idGame' => $paramValue,
+				];
+			} else {
+				$where = [
+					$paramName => $paramValue,
+				];
+			}
+		}
+
+		$languages = $this->select($fields, $where, [], $join);
+
+		return $languages;
+	}
+
+	/**
+	 * Insert a new language in database.
+	 * If the language already exists, return the existing language's ID.
+	 *
+	 * @param $datas string Language's name
+	 * @return $id int Language's ID
+	 */
+	public function insertLanguage($datas)
+	{
+		/*
+		 * Check that the language doesn't already exist.
+		 * If so, return this ID
+		 */
+		if ($existingLanguage = $this->findBy('language', $datas['language'])) {
+			return $existingLanguage;
+		} else {
+			try {
+				$pdo  = $this->db;
+				$stmt = $pdo->prepare('INSERT INTO `language` (`language`) 
+									   VALUES (:language)');
+				$stmt->bindParam(':language', $datas['language'], PDO::PARAM_STR);
+				$stmt->execute();
+
+				return $pdo->lastInsertId();
+			} catch (PDOException $e) {
+				return false;
+			} catch (Exception $e) {
+				return false;
+			}
+		}
+	}
+
+	/**
+	 * Update language
+	 *
+	 * @param $idLanguage int Language's ID
+	 * @param $language string Language's name
+	 */
+	public function updateLanguage($idLanguage, $datas)
+	{
+		try {
+			$pdo  = $this->db;
+			$stmt = $pdo->prepare('UPDATE `language` 
+								   SET `language` = :language 
+								   WHERE `idLanguage` =  :idLanguage');
+			$stmt->bindParam(':language', $datas['language'], PDO::PARAM_STR);
+			$stmt->bindParam(':idLanguage', $idLanguage, PDO::PARAM_INT);
+			$stmt->execute();
+
+			/*
+			 * Check that the update was performed on an existing language.
+			 * MySQL won't send any error as, regarding to him, the request is correct, so we have to handle it manually.
+			 */
+			return $stmt->rowCount();
+		} catch (PDOException $e) {
+			return false;
+		} catch (Exception $e) {
+			return false;
+		}
+	}
+}
