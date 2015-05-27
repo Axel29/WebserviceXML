18887acf73a08a76b038b4769258742ad8ae466b
Axel29 <axel.bouaziz@hotmail.fr>
2015-05-27 21:00:54 +0200

    CRUD Themes

---
+++ controllers/ThemeController.php
@@ -0,0 +1,233 @@
+<?php
+class ThemeController extends BaseController
+{
+	/**
+	 * @var $id int Theme's ID
+	 */
+	private $id = null;
+
+	/**
+	 * Redirect the request to the matching method regarding the request method
+	 * Route: /theme/index/id/{id}
+	 *
+	 * @param $id int ID of the theme. Used for POST, PUT and DELETE methods
+	 */
+	public function indexAction($id = null)
+	{
+		if ($id) {
+			$this->setId($id);
+		}
+
+		switch ($this->getRequestMethod()) {
+			case 'GET':
+				$this->show();
+				break;
+			case 'POST':
+				$this->add();
+				break;
+			case 'PUT':
+				$this->update();
+				break;
+			case 'DELETE':
+				$this->delete();
+				break;
+			
+			default:
+				$this->show();
+				break;
+		}
+	}
+
+	/**
+	 * Show the full themes list or a specific theme by it's ID
+	 *
+	 * @param $id int Theme's ID
+	 */
+	public function show()
+	{
+		$themeModel = new Theme();
+
+		// Show the full theme list or a specific theme by it's ID
+		$datas = $themeModel->findBy('idTheme', $this->getId());
+
+		$this->xml = $this->generateXml($datas)->asXML();
+
+		if ($errors = $this->validateXML($this->xml)) {
+			$this->exitError(400, $errors);
+		} else {
+			$this->loadLayout('xml');
+			echo $this->xml;
+		}
+	}
+
+	/**
+	 * Add new theme
+	 * Route: /theme/add
+	 */
+	public function add()
+	{
+		// Security check for the request method
+		if ($this->getRequestMethod() != 'POST') {
+			$this->exitError(405, 'Only POST methods are allowed.');
+			return;
+		}
+
+		// Check every required field
+		$requiredFields = [
+			'theme' => 'string',
+		];
+
+		$this->checkRequiredFields($requiredFields, $_POST);
+
+		$themeModel    = new Theme();
+		$insertedTheme = $themeModel->insertTheme($_POST);
+
+		if ($insertedTheme) {
+			$this->sendStatus(201);
+			return;
+		} else {
+			$this->exitError(400, 'An error has occurred. Please try again.');
+		}
+	}
+
+	/**
+	 * Update theme
+	 * Route: /theme/index/id/{id}
+	 */
+	public function update()
+	{
+		// Security check for the request method
+		if ($this->getRequestMethod() != 'PUT') {
+			$this->exitError(405, 'Only PUT methods are allowed.');
+			return;
+		}
+
+		// Get PUT datas
+		parse_str(file_get_contents("php://input"), $_PUT);
+		if (!$this->getId()) {
+			$this->exitError(400, "'id' must be specified.");
+		}
+
+		// Check every required field
+		$requiredFields = [
+			'theme' => 'string',
+		];
+
+		$this->checkRequiredFields($requiredFields, $_PUT);
+
+		if (!isset($_PUT['theme'])) {
+			$this->exitError(400, "'theme' must be specified.");
+		}
+		if (!is_string($_PUT['theme'])) {
+			$this->exitError(400, "'theme' must be a valid string.");
+		}
+
+		$themeModel  = new Theme();
+		$updateTheme = $themeModel->updateTheme($this->getId(), $_PUT);
+
+		if ($updateTheme) {
+			$this->sendStatus(204);
+			return;
+		} else {
+			$this->exitError(400, 'An error has occurred. Please try again.');
+		}
+	}
+
+	/**
+	 * Delete theme. Forbiddent action.
+	 * Route: /theme/index/id/{id}
+	 *
+	 * @param $id int Theme's ID to delete
+	 */
+	public function delete()
+	{		
+		$this->exitError(405, 'Themes deletion is not allowed.');
+	}
+
+	/**
+	 * Generate XML from database.
+	 * Can generate either the entire database's XML, either only one theme's XML.
+	 *
+	 * @param array $theme Theme to insert in the XML
+	 * @return SimpleXMLElement $themes List of themes or theme
+	 */
+	public function generateXml($themes = [])
+	{
+		$list = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><themes/>');
+		foreach ($themes as $theme) {
+			$themeNode = $list->addChild('theme', $theme['theme']);
+			$themeNode->addAttribute('id', $theme['idTheme']);
+		}
+		return $list;
+	}
+
+	/**
+	 * Validate XML from XSD
+	 *
+	 * @param $xml SimpleXMLElement XML to validate
+	 * @return $result string Errors to display or empty string
+	 */
+	public function validateXML($xml)
+	{
+		// Enable user error handling
+		libxml_use_internal_errors(true);
+
+		$domDocument = new DOMDocument();
+		$domDocument->loadXML($xml);
+
+		$result = '';
+
+		if (!$domDocument->schemaValidate(SCHEMAS_PATH . 'themes.xsd')) {
+			$errors = libxml_get_errors();
+
+			foreach ($errors as $error) {
+				$result = "<br>\n";
+				switch ($error->level) {
+					case LIBXML_ERR_WARNING:
+						$result .= "<strong>Warning $error->code</strong>: ";
+					break;
+					case LIBXML_ERR_ERROR:
+						$result .= "<strong>Error $error->code</strong>: ";
+					break;
+					case LIBXML_ERR_FATAL:
+						$result .= "<strong>Fatal Error $error->code</strong>: ";
+					break;
+				}
+
+				$result .= trim($error->message);
+
+				if ($error->file) {
+					$result .= " in <strong>$error->file</strong>";
+				}
+				$result .= " on line <strong>$error->line</strong>\n";
+			}
+			libxml_clear_errors();
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Get the ID
+	 *
+	 * @return int
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	/**
+	 * Set the ID
+	 *
+	 * @param $id int
+	 */
+	public function setId($id)
+	{
+		if ($this->isInt($id)) {
+			$this->id = $id;
+		} else {
+			$this->exitError(400, sprintf('The ID must be an integer. %s given', gettype($id)));
+		}
+	}
+}
---
+++ model/Theme.php
@@ -0,0 +1,109 @@
+<?php
+class Theme extends BaseModel
+{
+	/**
+	 * Retrieve every available themes or themes by some param
+	 *
+	 * @param $paramName string Param's name to find by
+	 * @param $paramValue mixed Param's value
+	 * @return $themes array
+	 */
+	public function findBy($paramName = null, $paramValue = null)
+	{
+		$this->table = 'theme t';
+
+		$fields = ['`idTheme`', '`theme`'];
+		
+		$where = [];
+		$join  = [];
+		if ($paramName && $paramValue) {
+			if ($paramName == 'idGame') {
+				$join = [
+					[
+						'type'  => 'INNER JOIN',
+						'table' => 'game_has_theme ght',
+						'on'    => 't.idTheme = ght.theme_idTheme',
+					],
+					[
+						'type'  => 'INNER JOIN',
+						'table' => 'game g',
+						'on'    => 'g.idGame = ght.game_idGame',
+					],
+				];
+
+				$where = [
+					'ght.game_idGame' => $paramValue,
+				];
+			} else {
+				$where = [
+					$paramName => $paramValue,
+				];
+			}
+		}
+
+		$themes = $this->select($fields, $where, [], $join);
+
+		return $themes;
+	}
+
+	/**
+	 * Insert a new theme in database.
+	 * If the theme already exists, return the existing theme's ID.
+	 *
+	 * @param $datas string Theme's name
+	 * @return $id int Theme's ID
+	 */
+	public function insertTheme($datas)
+	{
+		/*
+		 * Check that the theme doesn't already exist.
+		 * If so, return this ID
+		 */
+		if ($existingTheme = $this->findBy('theme', $datas['theme'])) {
+			return $existingTheme;
+		} else {
+			try {
+				$pdo  = $this->db;
+				$stmt = $pdo->prepare('INSERT INTO `theme` (`theme`) 
+									   VALUES (:theme)');
+				$stmt->bindParam(':theme', $datas['theme'], PDO::PARAM_STR);
+				$stmt->execute();
+
+				return $pdo->lastInsertId();
+			} catch (PDOException $e) {
+				return false;
+			} catch (Exception $e) {
+				return false;
+			}
+		}
+	}
+
+	/**
+	 * Update theme
+	 *
+	 * @param $idTheme int Theme's ID
+	 * @param $theme string Theme's name
+	 */
+	public function updateTheme($idTheme, $datas)
+	{
+		try {
+			$pdo  = $this->db;
+			$stmt = $pdo->prepare('UPDATE `theme` 
+								   SET `theme` = :theme 
+								   WHERE `idTheme` =  :idTheme');
+			$stmt->bindParam(':theme', $datas['theme'], PDO::PARAM_STR);
+			$stmt->bindParam(':idTheme', $idTheme, PDO::PARAM_INT);
+			$stmt->execute();
+
+			/*
+			 * Check that the update was performed on an existing theme.
+			 * MySQL won't send any error as, regarding to him, the request is correct, so we have to handle it manually.
+			 */
+			return $stmt->rowCount();
+		} catch (PDOException $e) {
+			return false;
+		} catch (Exception $e) {
+			return false;
+		}
+	}
+}
