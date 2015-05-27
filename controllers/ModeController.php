50b6166fcd5579c751cab8af70bbd8f06b39e80b
Axel29 <axel.bouaziz@hotmail.fr>
2015-05-27 20:03:44 +0200

    CRUD Modes

---
+++ controllers/ModeController.php
@@ -0,0 +1,248 @@
+<?php
+/**
+ * @todo Vérifier que l'ID de la console existe avant d'insérer ou de mettre à jour.
+ */
+class ModeController extends BaseController
+{
+	/**
+	 * @var $id int Mode's ID
+	 */
+	private $id = null;
+
+	/**
+	 * @var $requiredFields array Required fields and their types for insert / update
+	 */
+	private $requiredFields = [
+		'mode' => 'string',
+	];
+
+	/**
+	 * Redirect the request to the matching method regarding the request method
+	 * Route: /mode/index/id/{id}
+	 *
+	 * @param $id int ID of the mode. Used for POST, PUT and DELETE methods
+	 */
+	public function indexAction($id = null)
+	{
+		if ($id) {
+			$this->setId($id);
+		}
+
+		switch ($this->getRequestMethod()) {
+			case 'GET':
+				$this->show();
+				break;
+			case 'POST':
+				$this->add();
+				break;
+			case 'PUT':
+				$this->update();
+				break;
+			case 'DELETE':
+				$this->delete();
+				break;
+			
+			default:
+				$this->show();
+				break;
+		}
+	}
+
+	/**
+	 * Show the full modes list or a specific mode by it's ID
+	 *
+	 * @param $id int Mode's ID
+	 */
+	public function show()
+	{
+		$modeModel = new Mode();
+
+		// Show the full mode list or a specific mode by it's ID
+		$datas = $modeModel->findBy('idMode', $this->getId());
+
+		$this->xml = $this->generateXml($datas)->asXML();
+
+		if ($errors = $this->validateXML($this->xml)) {
+			$this->exitError(400, $errors);
+		} else {
+			$this->loadLayout('xml');
+			echo $this->xml;
+		}
+	}
+
+	/**
+	 * Add new mode
+	 * Route: /mode/add
+	 */
+	public function add()
+	{
+		// Security check for the request method
+		if ($this->getRequestMethod() != 'POST') {
+			$this->exitError(405, 'Only POST methods are allowed.');
+			return;
+		}
+
+		// Check every required field
+		$this->checkRequiredFields($this->requiredFields, $_POST);
+
+		$modeModel    = new Mode();
+		$insertedMode = $modeModel->insertMode($_POST);
+
+		if ($insertedMode) {
+			$this->sendStatus(201);
+			return;
+		} else {
+			$this->exitError(400, 'An error has occurred. Please try again.');
+		}
+	}
+
+	/**
+	 * Update mode
+	 * Route: /mode/index/id/{id}
+	 */
+	public function update()
+	{
+		// Security check for the request method
+		if ($this->getRequestMethod() != 'PUT') {
+			$this->exitError(405, 'Only PUT methods are allowed.');
+			return;
+		}
+
+		// Get PUT datas
+		parse_str(file_get_contents("php://input"), $_PUT);
+		if (!$this->getId()) {
+			$this->exitError(400, "'id' must be specified.");
+		}
+
+		// Check every required field
+		$this->checkRequiredFields($this->requiredFields, $_PUT);
+
+		$modeModel  = new Mode();
+		$updatedMode = $modeModel->updateMode($this->getId(), $_PUT);
+
+		if ($updatedMode) {
+			$this->sendStatus(204);
+			return;
+		} else {
+			$this->exitError(400, 'An error has occurred. Please try again.');
+		}
+	}
+
+	/**
+	 * Delete mode
+	 * Route: /mode/index/id/{id}
+	 *
+	 * @param $id int Mode's ID to delete
+	 */
+	public function delete()
+	{		
+		// Security check for the request method
+		if (!$this->getRequestMethod() == 'DELETE') {
+			$this->exitError(405, 'Only DELETE methods are allowed.');
+			return;
+		}
+
+		if (!$this->getId()) {
+			$this->exitError(400, "'id' must be specified.");
+		}
+
+		$modeModel   = new Mode();		
+		$deletedMode = $modeModel->deleteMode($this->getId());
+
+        if ($deletedMode) {
+            $this->sendStatus(204);
+        } else {
+            $this->exitError(400, 'An error has occured. Please try again.');
+        }
+	}
+
+	/**
+	 * Generate XML from database.
+	 * Can generate either the entire database's XML, either only one mode's XML.
+	 *
+	 * @param array $mode Mode to insert in the XML
+	 * @return SimpleXMLElement $modes List of modes or mode
+	 */
+	public function generateXml($modes = [])
+	{
+		$list = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><modes/>');
+		foreach ($modes as $mode) {
+			$modeNode = $list->addChild('mode', $mode['mode']);
+			$modeNode->addAttribute('id', $mode['idMode']);
+		}
+		// $this->loadLayout('xml');
+		// echo($list->asXML());
+		// die;
+		return $list;
+	}
+
+	/**
+	 * Validate XML from XSD
+	 *
+	 * @param $xml SimpleXMLElement XML to validate
+	 * @return $result string Errors to display or empty string
+	 */
+	public function validateXML($xml)
+	{
+		// Enable user error handling
+		libxml_use_internal_errors(true);
+
+		$domDocument = new DOMDocument();
+		$domDocument->loadXML($xml);
+
+		$result = '';
+
+		if (!$domDocument->schemaValidate(SCHEMAS_PATH . 'modes.xsd')) {
+			$errors = libxml_get_errors();
+
+			foreach ($errors as $error) {
+				$result = "<br>\n";
+				switch ($error->level) {
+					case LIBXML_ERR_WARNING:
+						$result .= "<strong>Warning $error->code</strong>: ";
+					break;
+					case LIBXML_ERR_ERROR:
+						$result .= "<strong>Error $error->code</strong>: ";
+					break;
+					case LIBXML_ERR_FATAL:
+						$result .= "<strong>Fatal Error $error->code</strong>: ";
+					break;
+				}
+
+				$result .= trim($error->message);
+
+				if ($error->file) {
+					$result .= " in <strong>$error->file</strong>";
+				}
+				$result .= " on line <strong>$error->line</strong>\n";
+			}
+			libxml_clear_errors();
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Get the ID
+	 *
+	 * @return int
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	/**
+	 * Set the ID
+	 *
+	 * @param $id int
+	 */
+	public function setId($id)
+	{
+		if ($this->isInt($id)) {
+			$this->id = $id;
+		} else {
+			$this->exitError(400, sprintf("'id' must be an integer, %s given", gettype($id)));
+		}
+	}
+}
---
+++ model/Mode.php
@@ -0,0 +1,125 @@
+<?php
+class Mode extends BaseModel
+{
+	/**
+	 * Retrieve every available modes or modes by some param
+	 *
+	 * @param $paramName string Param's name to find by
+	 * @param $paramValue mixed Param's value
+	 * @return $modes array
+	 */
+	public function findBy($paramName = null, $paramValue = null)
+	{
+		$this->table = 'mode m';
+		
+		$fields = [
+			'`idMode`',
+			'`mode`',
+		];
+
+		$where = [];
+		$join  = [];
+		if ($paramName && $paramValue) {
+			if ($paramName == 'idConsole') {
+				$join = [
+					[
+						'type'  => 'INNER JOIN',
+						'table' => 'console_has_mode chm',
+						'on'    => 'm.idMode = chm.support_idMode',
+					],
+					[
+						'type'  => 'INNER JOIN',
+						'table' => 'console c',
+						'on'    => 'c.idConsole = chm.console_idConsole',
+					],
+				];
+
+				$where = [
+					'chm.console_idConsole' => $paramValue,
+				];
+			} else {
+				$where = [
+					$paramName => $paramValue,
+				];
+			}
+		}
+
+		$modes = $this->select($fields, $where);
+
+		return $modes;
+	}
+
+	/**
+	 * Insert a new mode in database.
+	 *
+	 * @param $datas array Mode's name
+	 * @return $id int Mode's ID
+	 */
+	public function insertMode($datas)
+	{
+		try {
+			$pdo  = $this->db;
+			$stmt = $pdo->prepare('INSERT INTO `mode` (`mode`) 
+								   VALUES (:mode)');
+			$stmt->bindParam(':mode', $datas['mode'], PDO::PARAM_STR);
+			$stmt->execute();
+
+			return $pdo->lastInsertId();
+		} catch (PDOException $e) {
+			echo $e->getMessage();
+			return false;
+		} catch (Exception $e) {
+			return false;
+		}
+	}
+
+	/**
+	 * Update mode
+	 *
+	 * @param $idMode int Mode's ID
+	 * @param $datas array Datas to update
+	 */
+	public function updateMode($idMode, $datas)
+	{
+		try {
+			$pdo  = $this->db;
+			$stmt = $pdo->prepare('UPDATE `mode`
+								   SET `mode`             = :mode
+								   WHERE `idMode` =  :idMode');
+			$stmt->bindParam(':mode', $datas['mode'], PDO::PARAM_STR);
+			$stmt->bindParam(':idMode', $idMode, PDO::PARAM_INT);
+			$stmt->execute();
+
+			return $stmt->rowCount();
+		} catch (PDOException $e) {
+			echo $e->getMessage();
+			return false;
+		} catch (Exception $e) {
+			echo $e->getMessage();
+			return false;
+		}
+	}
+
+	/**
+	 * Delete a mode by it's ID
+	 *
+	 * @param $id int Mode's ID
+	 */
+	public function deleteMode($idMode)
+	{
+		try {
+			$pdo  = $this->db;
+			$stmt = $pdo->prepare('DELETE 
+								   FROM `mode` 
+								   WHERE `idMode` =  :idMode');
+			$stmt->bindParam(':idMode', $idMode, PDO::PARAM_INT);
+			$stmt->execute();
+
+			return $stmt->rowCount();
+		} catch (PDOException $e) {
+			return false;
+		} catch (Exception $e) {
+			return false;
+		}
+	}
+}
