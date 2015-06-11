c28d7730f8dd18b9fa00a53884223369317e029d
Axel29 <axel.bouaziz@hotmail.fr>
2015-06-11 08:40:46 +0200

    Pagination Console

--- model/Console.php
+++ model/Console.php
@@ -6,9 +6,11 @@ class Console extends BaseModel
 	 *
 	 * @param string $paramName Param's name to find by
 	 * @param mixed $paramValue Param's value
+	 * @param bool $notPaginated Should paginate or not
+	 * @param int $page Current page
 	 * @return array $consoles Collection of consoles
 	 */
-	public function findBy($paramName = null, $paramValue = null)
+	public function findBy($paramName = null, $paramValue = null, $notPaginated = true, $page = 1)
 	{
 		$this->table = 'console';
 		
@@ -31,7 +33,13 @@ class Console extends BaseModel
 			];
 		}
 
-		$consoles = $this->select($fields, $where);
+		if ($notPaginated) {
+			$limit = '';
+		} else {
+			$limit = $page - 1 . ', ' . $this->getLimit();
+		}
+
+		$consoles = $this->select($fields, $where, [], [], [], $limit);
 
 		return $consoles;
 	}
