1d96b7d1573618488faf3c7593a714d9b1217cf9
Axel29 <axel.bouaziz@hotmail.fr>
2015-06-11 09:27:22 +0200

    Pagination Test

--- controllers/TestController.php
+++ controllers/TestController.php
@@ -1,43 +1,6 @@
 <?php
-class TestController extends BaseController
+class TestController extends CRUD
 {
-	/**
-	 * @var $id int Test's ID
-	 */
-	private $id = null;
-
-	/**
-	 * Redirect the request to the matching method regarding the request method
-	 * Route: /test/index/id/{id}
-	 *
-	 * @param $id int ID of the test. Used for POST, PUT and DELETE methods
-	 */
-	public function indexAction($id = null)
-	{
-		if ($id) {
-			$this->setId($id);
-		}
-
-		switch ($this->getRequestMethod()) {
-			case 'GET':
-				$this->show();
-				break;
-			case 'POST':
-				$this->add();
-				break;
-			case 'PUT':
-				$this->update();
-				break;
-			case 'DELETE':
-				$this->delete();
-				break;
-			
-			default:
-				$this->show();
-				break;
-		}
-	}
-
 	/**
 	 * Show the full tests list or a specific test by it's ID
 	 *
@@ -46,21 +9,26 @@ class TestController extends BaseController
 	public function show()
 	{
 		$testModel = new Test();
+		$page      = $this->getPage() ? $this->getPage() : 1;
 
 		// Show the full test list or a specific test by it's ID
-		$datas = $testModel->findBy('idTest', $this->getId());
+		$datas = $testModel->findBy('idTest', $this->getId(), false, $page);
 
 		if ($datas) {
 			$this->xml = $this->generateXml($datas)->asXML();
 
-			if ($errors = $this->validateXML($this->xml)) {
+			if ($errors = $this->validateXML($this->xml, SCHEMAS_PATH . 'tests.xsd')) {
 				$this->exitError(400, $errors);
 			} else {
 				$this->loadLayout('xml');
 				echo $this->xml;
 			}
 		} else {
-			$this->exitError(400, "This test doesn't exist.");
+			if ($this->getId()) {
+				$this->exitError(400, "The ID you specified can't be found.");
+			} else {
+				$this->exitError(400, "The page you specified doesn't exist.");
+			}
 		}
 	}
 
@@ -227,79 +195,12 @@ class TestController extends BaseController
 				}
 			}
 		}
-		// $this->loadLayout('xml');
-		// echo($list->asXML());
-		// die;
+
+		if (!$this->getId()) {
+			$nextPrevPagesUrls = $this->getNextPrevPages('Test');
+			$list->addChild('prev', $nextPrevPagesUrls['prev']);
+			$list->addChild('next', $nextPrevPagesUrls['next']);
+		}
 		return $list;
-	}
-
-	/**
-	 * Validate XML from XSD
-	 *
-	 * @param $xml SimpleXMLElement XML to validate
-	 * @return $result string Errors to display or empty string
-	 */
-	public function validateXML($xml)
-	{
-		// Enable user error handling
-		libxml_use_internal_errors(true);
-
-		$domDocument = new DOMDocument();
-		$domDocument->loadXML($xml);
-
-		$result = '';
-
-		if (!$domDocument->schemaValidate(SCHEMAS_PATH . 'tests.xsd')) {
-			$errors = libxml_get_errors();
-
-			foreach ($errors as $error) {
-				$result = "<br>\n";
-				switch ($error->level) {
-					case LIBXML_ERR_WARNING:
-						$result .= "<strong>Warning $error->code</strong>: ";
-					break;
-					case LIBXML_ERR_ERROR:
-						$result .= "<strong>Error $error->code</strong>: ";
-					break;
-					case LIBXML_ERR_FATAL:
-						$result .= "<strong>Fatal Error $error->code</strong>: ";
-					break;
-				}
-
-				$result .= trim($error->message);
-
-				if ($error->file) {
-					$result .= " in <strong>$error->file</strong>";
-				}
-				$result .= " on line <strong>$error->line</strong>\n";
-			}
-			libxml_clear_errors();
-		}
-
-		return $result;
-	}
-
-	/**
-	 * Get the ID
-	 *
-	 * @return int
-	 */
-	public function getId()
-	{
-		return $this->id;
-	}
-
-	/**
-	 * Set the ID
-	 *
-	 * @param $id int
-	 */
-	public function setId($id)
-	{
-		if ($this->isInt($id)) {
-			$this->id = $id;
-		} else {
-			$this->exitError(400, sprintf("'id' must be an integer, %s given", gettype($id)));
-		}
 	}
 }
